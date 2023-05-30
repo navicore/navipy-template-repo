a812b6cb947486b4bf2a6b1cf09f8832f84f967b
Ed Sweeney <ed@onextent.com>
2023-05-29 17:10:36 -0700

    demo

--- src/CHANGE_ME/__main__.py
+++ src/CHANGE_ME/__main__.py
@@ -9,9 +9,11 @@ def main():
     entry point
     """
     parser = argparse.ArgumentParser(description='do something')
-    parser.add_argument('--compact-json', action='store_true', help='Generate Compact JSON')
+    parser.add_argument('--demo', action='store_true', help='Generate Demo Msg')
     args = parser.parse_args()
-    print("ha ha")
+
+    if args.demo:
+        print("Demo")
 
 if __name__ == "__main__":
     main()
